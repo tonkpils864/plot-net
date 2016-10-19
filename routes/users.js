dc095448bbd49e2fe57b57c16700ca1ca050c710
tonkpils864 <st.stathatos@gmail.com>
2016-10-19 17:35:44 +0300

    Logout and restricted routes

--- routes/users.js
+++ routes/users.js
@@ -89,5 +89,11 @@ router.post('/login', passport.authenticate('local',{successRedirect:'/',failure
       res.redirect('/');
   });
 
+router.get('/logout', function(req,res) {
+    req.logout();
+    req.flash('success_msg', 'You are logged out.');
+    res.redirect('/users/login');
+})
+
 
 module.exports= router;
